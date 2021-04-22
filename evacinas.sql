a8c0a9ad143385e101754867334c069109b077a8
Thiago <thi4golaet@gmail.com>
2021-04-22 18:38:26 -0300

    Create table sem os constriants

--- evacinas.sql
+++ evacinas.sql
@@ -13,19 +13,14 @@ CREATE TABLE CIDADAO
     estado_ci CHAR(2) NOT NULL,
     cidade_ci VARCHAR(100) NOT NULL,
     bairro_ci VARCHAR(100) NOT NULL,
-    endereco_ci VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_ci PRIMARY KEY (cpf_ci)
+    endereco_ci VARCHAR(100) NOT NULL
 );
 
 
 CREATE TABLE FUNCIONARIO 
 (
 	cod_fu INT NOT NULL,
-    cpf_ci_fu CHAR(11) NOT NULL,
-    CONSTRAINT pk_fu PRIMARY KEY (cod_fu),
-    CONSTRAINT fk_ci_fu FOREIGN KEY (cpf_ci_fu) REFERENCES CIDADAO(cpf_ci)
-    ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT cpf_ci_fu_uk UNIQUE (cpf_ci_fu)
+    cpf_ci_fu CHAR(11) NOT NULL
 );
 
 CREATE TABLE POSTO_VACINACAO
@@ -36,16 +31,13 @@ CREATE TABLE POSTO_VACINACAO
 	cidade_po VARCHAR(100) NOT NULL,
     bairro_po VARCHAR(100) NOT NULL,
     endereco_po VARCHAR(100) NOT NULL,
-    telefone_po CHAR(10) NOT NULL,
-    CONSTRAINT pk_po PRIMARY KEY (cod_po),
-    CONSTRAINT endereco_po_uk UNIQUE (estado_po, cidade_po, bairro_po, endereco_po)
+    telefone_po CHAR(10) NOT NULL
 );
 
 CREATE TABLE PRODUTOR_VACINAS
 (
 	cod_pr INT NOT NULL,
-    nome_pr VARCHAR(100) UNIQUE NOT NULL,
-    CONSTRAINT pk_pr PRIMARY KEY (cod_pr)
+    nome_pr VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE LOTE
@@ -54,32 +46,20 @@ CREATE TABLE LOTE
     cod_pr_lo INT NOT NULL,
     cod_va_lo INT NOT NULL, 
     data_fabricacao_lo DATE NOT NULL,
-    data_validade_lo DATE NOT NULL,
-    CONSTRAINT pk_lo PRIMARY KEY (cod_lo, cod_pr_lo),
-    CONSTRAINT fk_pr_lo FOREIGN KEY (cod_pr_lo) REFERENCES PRODUTOR_VACINAS(cod_pr)
-    ON UPDATE CASCADE ON DELETE RESTRICT,
-    CONSTRAINT fk_va_lo FOREIGN KEY (cod_va_lo) REFERENCES VACINA(cod_va)
-    ON UPDATE CASCADE ON DELETE RESTRICT
+    data_validade_lo DATE NOT NULL
 );
 
 CREATE TABLE FRASCO
 (
 	cod_fr INT NOT NULL,
     cod_lo_fr INT NOT NULL,
-    cod_po_fr INT NOT NULL,
-    CONSTRAINT pk_fr PRIMARY KEY (cod_fr, cod_lo_fr),
-    CONSTRAINT fk_lo_fr FOREIGN KEY (cod_lo_fr) REFERENCES LOTE(cod_lo)
-    ON UPDATE CASCADE ON DELETE RESTRICT,
-    CONSTRAINT fk_po_fr FOREIGN KEY (cod_po_fr) REFERENCES POSTO_VACINACAO(cod_po)
-    ON UPDATE CASCADE ON DELETE RESTRICT,
-    CONSTRAINT unq_registro_lo UNIQUE (registro_fr, cod_lo_fr)
+    cod_po_fr INT NOT NULL
 );
 
 CREATE TABLE PATOGENO 
 (
 	cod_pa INT NOT NULL,
-    nome_pa VARCHAR(100) NOT NULL,
-    CONSTRAINT pk_pa PRIMARY KEY (cod_pa)
+    nome_pa VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE VACINA
@@ -88,30 +68,19 @@ CREATE TABLE VACINA
     nome_va VARCHAR(100) NOT NULL,
     laboratorio_va VARCHAR(100) NOT NULL,
 	doses_necessarias_va INT NOT NULL,
-    intervalo_doses_va INT,
-    CONSTRAINT pk_va PRIMARY KEY (cod_va)
+    intervalo_doses_va INT
 );
 
 CREATE TABLE IMUNIZA_CONTRA 
 (
 	cod_va_ic INT NOT NULL,
-    cod_pa_ic INT NOT NULL,
-    CONSTRAINT pk_ic PRIMARY KEY (cod_va_ic, cod_pa_ic),
-    CONSTRAINT fk_va_ic FOREIGN KEY (cod_va_ic) REFERENCES VACINA(cod_va)
-    ON UPDATE CASCADE ON DELETE RESTRICT,
-    CONSTRAINT fk_pa_ic FOREIGN KEY (cod_pa_ic) REFERENCES PATOGENO (cod_pa)
-    ON UPDATE CASCADE ON DELETE RESTRICT
+    cod_pa_ic INT NOT NULL
 );
 
 CREATE TABLE TRABALHA_EM
 (
 	cod_fu_te INT NOT NULL,
-    cod_po_te INT NOT NULL,
-    CONSTRAINT pk_te PRIMARY KEY (cod_fu_te, cod_po_te),
-    CONSTRAINT fk_fu_te FOREIGN KEY (cod_fu_te) REFERENCES FUNCIONARIO(cod_fu)
-    ON UPDATE CASCADE ON DELETE RESTRICT,
-    CONSTRAINT fk_po_te FOREIGN KEY (cod_po_te) REFERENCES POSTO_VACINACAO(cod_po)
-    ON UPDATE CASCADE ON DELETE RESTRICT
+    cod_po_te INT NOT NULL
 );
 
 CREATE TABLE APLICA
@@ -119,12 +88,5 @@ CREATE TABLE APLICA
 	cpf_ci_ap CHAR(11) NOT NULL,
     cod_fu_ap INT NOT NULL,
     cod_fr_ap INT NOT NULL,
-    data_ap DATE NOT NULL,
-    CONSTRAINT pk_ap PRIMARY KEY(cpf_ci_ap, cod_fu_ap, cod_fr_ap),
-    CONSTRAINT fk_ci_ap FOREIGN KEY (cpf_ci_ap) REFERENCES CIDADAO(cpf_ci)
-    ON UPDATE CASCADE ON DELETE RESTRICT,
-    CONSTRAINT fk_fu_ap FOREIGN KEY (cod_fu_ap) REFERENCES FUNCIONARIO(cod_fu)
-    ON UPDATE CASCADE ON DELETE RESTRICT,
-    CONSTRAINT fk_fr_ap FOREIGN KEY (cod_fr_ap) REFERENCES FRASCO(cod_fr)
-    ON UPDATE CASCADE ON DELETE RESTRICT
+    data_ap DATE NOT NULL
 );
